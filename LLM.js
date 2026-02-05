99f92ee1f9511d5e8465f2578c07f17b0c2e78fd
Deeksha Jain <deeksha@metaversitytechnologies.com>
2026-02-05 22:27:40 +0530

    Gemini LLM project

---
+++ LLM.js
@@ -0,0 +1,19 @@
+import { GoogleGenAI } from '@google/genai';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const GEMINI_API_KEY = process.env.GEMINI_API;
+
+const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: 'gemini-2.5-flash',
+    contents: 'Why is the sky blue?',
+  });
+
+  console.log(response.text);
+}
+
+main();
